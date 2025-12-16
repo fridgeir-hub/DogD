6b495e8242bfc5c2d2d1a9eba3c64a9d38cf6c8b
fridgeir-hub <fridgeir@lundarskoli.is>
2025-12-16 14:00:25 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'dnd-dice-v1.0.0';
-const VERSION = '1.0.0';
+const CACHE_NAME = 'dnd-dice-v1.1.0';
+const VERSION = '1.1.0';
 
 // Get the base path dynamically
 const getBasePath = () => {
